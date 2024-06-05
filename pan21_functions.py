252ff98729ed4b6419b74b7ea2836575c5ddc61b
tlazor <tjlazor@vt.edu>
2024-06-05 20:44:29 +0200

    simplify base pydataset

--- pan21_functions.py
+++ pan21_functions.py
@@ -18,6 +18,7 @@ from transformers import BertTokenizer, BertModel
 from joblib import Memory
 
 memory = Memory(".cache", verbose=0)
+# memory = Memory(".cache_test", verbose=1)
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -28,7 +29,7 @@ def get_tokenizer_model():
 
     return bert_tokenizer, bert_model
 
-# @memory.cache
+@memory.cache
 def read_problem_files(problem_folder, start=0, stop=None):
     # print(f"{start=} {stop=}")
     problems = []
@@ -124,114 +125,105 @@ def flatten_problems(problems_list, squeeze=False):
         # [print(f"{pair=}") for problem in problems_list for pair in problem]
         return [pair.squeeze(0) if squeeze else pair for problem in problems_list for pair in problem]
 
+from itertools import islice
+bert_tokenizer, bert_model = get_tokenizer_model()
+
+@memory.cache
+def get_problem_embeddings(problems, max_input_length, verbose=False):
+    def get_paragraph_pairs(problem_text, max_input_length):
+        def get_embeddings(paragraph):
+            inputs = bert_tokenizer(paragraph, return_tensors='pt', padding=True, truncation=True)
+
+            # Move inputs to GPU
+            inputs = {key: value.to(DEVICE) for key, value in inputs.items()}
+
+            # Step 4: Generate embeddings
+            with torch.no_grad():
+                outputs = bert_model(**inputs)
+
+            # The last hidden state contains the embeddings for each token
+            return outputs.last_hidden_state
+        # print(problem_text)
+        paragraph_embeddings = [pad_paragraph(get_embeddings(para[:max_input_length]), max_input_length) for para in problem_text]
+        # print(f"{[paras.shape for paras in paragraph_embeddings]=}")
+        # print(f"{[x.shape for x in paragraph_embeddings]}")
+        # print(f"{len(paragraph_embeddings)=}")
+        pairs = itertools.combinations(paragraph_embeddings, 2)
+        return [torch.flatten(torch.stack(pair, dim=2), start_dim=1, end_dim=2) for pair in pairs]
+    if verbose:
+        return [get_paragraph_pairs(problem_text, max_input_length) for problem_text in tqdm(problems)]
+    else:
+        return [get_paragraph_pairs(problem_text, max_input_length) for problem_text in problems]
+
 class Pan21PyDataset(PyDataset):
-    bert_tokenizer, bert_model = get_tokenizer_model()
-    
-    def get_problem_embeddings(problems, max_input_length, verbose=False):
-        def get_paragraph_pairs(problem_text, max_input_length):
-            def get_embeddings(paragraph):
-                inputs = Pan21PyDataset.bert_tokenizer(paragraph, return_tensors='pt', padding=True, truncation=True)
-
-                # Move inputs to GPU
-                inputs = {key: value.to(DEVICE) for key, value in inputs.items()}
-
-                # Step 4: Generate embeddings
-                with torch.no_grad():
-                    outputs = Pan21PyDataset.bert_model(**inputs)
-
-                # The last hidden state contains the embeddings for each token
-                return outputs.last_hidden_state
-            # print(problem_text)
-            paragraph_embeddings = [pad_paragraph(get_embeddings(para[:max_input_length]), max_input_length) for para in problem_text]
-            # print(f"{[paras.shape for paras in paragraph_embeddings]=}")
-            # print(f"{[x.shape for x in paragraph_embeddings]}")
-            # print(f"{len(paragraph_embeddings)=}")
-            pairs = itertools.combinations(paragraph_embeddings, 2)
-            return [torch.flatten(torch.stack(pair, dim=2), start_dim=1, end_dim=2) for pair in pairs]
-        if verbose:
-            return [get_paragraph_pairs(problem_text, max_input_length) for problem_text in tqdm(problems)]
-        else:
-            return [get_paragraph_pairs(problem_text, max_input_length) for problem_text in problems]
-
-    def __init__(self, x_path, y_set, file_path, max_input_length=256, batch_size=32,**kwargs):
+    def __init__(self, x_path, y_path, max_input_length=256, batch_size=128,**kwargs):
         super().__init__(**kwargs)
-        
 
-        self.x_path, self.y = x_path, read_ground_truth_files(y_set)
+        self.x_path = x_path
+        y = read_ground_truth_files(y_path)
+        self.task_3_y = get_task_3_ground_truth(get_simple_ground_truth(y, range(1, len(y))))
+        self.task_3_lens = [len(problem) for problem in self.task_3_y]
+        self.num_problems = len(self.task_3_y)
+        self.num_pairs = np.sum(self.task_3_lens)
+
         self.max_input_length = max_input_length
         self.batch_size = batch_size
-
-        self.file_path = Path(file_path)
-        self.file_path.mkdir(parents=True, exist_ok=True)
+        
+        self.length = math.ceil(self.num_pairs / self.batch_size)
 
     def __len__(self):
         # Return number of batches.
-        return math.ceil(len(self.y) / self.batch_size)
+        return self.length
 
-    def get_data(x_path, y, low_idx, high_idx, max_input_length):
-        # print(f"{self.x_path=}")
-        embeddings = Pan21PyDataset.get_problem_embeddings(read_problem_files(x_path, low_idx, high_idx), max_input_length)
-        gt = get_task_3_ground_truth(get_simple_ground_truth(y, range(low_idx+1, high_idx+1)))
-        
-        # print(f"{len(embeddings)=}")
-        # print(f"{len(gt)=}")
-        # print(f"{[len(embed) for embed in embeddings]=}")
-        # print(f"{[len(g) for g in gt]=}")
+    def get_data(self, low_problem_idx, high_problem_idx, low_idx, high_idx):
+        # print(f"{low_problem_idx=} {high_problem_idx=} {low_idx=} {high_idx=}")
+        if high_problem_idx < self.num_problems:
+            high_problem_idx += 1
+            # print(f"{self.x_path=}")
+            embeddings = get_problem_embeddings(read_problem_files(self.x_path, low_problem_idx, high_problem_idx), self.max_input_length)
 
-        # assert np.all([len(e) == len(g) for e, g in zip(embeddings, gt)]), f"{[len(embed) for embed in embeddings]=}\n" + f"{[len(g) for g in gt]=}"
+            batch_x = np.array(flatten_problems(embeddings, squeeze=True)[low_idx:high_idx])
+            batch_y = np.array(flatten_problems(self.task_3_y[low_problem_idx:high_problem_idx])[low_idx:high_idx])
+        else:
+            embeddings = get_problem_embeddings(read_problem_files(self.x_path, low_problem_idx, high_problem_idx+1), self.max_input_length)
 
-        batch_x = np.array(flatten_problems(embeddings, squeeze=True))
-        batch_y = np.array(flatten_problems(gt))
+            batch_x = np.array(flatten_problems(embeddings, squeeze=True)[low_idx:high_idx])
+            batch_y = np.array(flatten_problems(self.task_3_y[low_problem_idx])[low_idx:high_idx])
+
+        # print(f"{self.task_3_y[low_problem_idx:high_problem_idx]=}")
         return batch_x, batch_y
 
-    def to_file_helper(idx_path, i, x_path, y, batch_size, compress=False, max_input_length=256):
-        # print(f"{idx_path=}")
-        low = i * batch_size
+    def __getitem__(self, idx):
+        # print(f"{idx=}")
+        low = idx * self.batch_size
         # Cap upper bound at array length; the last batch may be smaller
         # if the total number of items is not a multiple of batch size.
-        high = min(low + batch_size, len(y))
+        high = min(low + self.batch_size, self.num_pairs)
         # print(f"{low=} {high=}")
-        batch_x, batch_y = Pan21PyDataset.get_data(x_path, y, low, high, max_input_length)
-        # print(f"{batch_x.shape=}")
-        if compress:
-            np.savez_compressed(idx_path, batch_x=batch_x, batch_y=batch_y)
-        else:
-            np.savez(idx_path, batch_x=batch_x, batch_y=batch_y)
 
-    def to_file(self, overwrite=False, compress=False):
-        free_memory = get_cuda_memory(0)
-        n_jobs = min(free_memory // 850, os.cpu_count() - 2)
-        # print(f"{n_jobs=} {free_memory=} {os.cpu_count()=}")
-        args_for_jobs = []
-        for i in range(len(self)):
-            idx_path = self.file_path / f"{i}.npz"
-            if not idx_path.exists() or overwrite:
-                args_for_jobs.append((idx_path, i, self.x_path, self.y, self.batch_size, compress, self.max_input_length))
-        
-        with parallel_config(backend='threading', n_jobs=n_jobs):
-            Parallel()(delayed(Pan21PyDataset.to_file_helper)(*args) for args in tqdm(args_for_jobs))
+        paragraph_pair_index = 0
+        low_problem_index = 0
+        high_problem_index = 0
+        previous_index_sum = 0
+        low_index_within_problem = 0
+        high_index_within_problem = 0
+        low_found = False
+        high_found = False
+        for i, pair_len in enumerate(self.task_3_lens):
+            paragraph_pair_index += pair_len
+            if not low_found and paragraph_pair_index > low:
+                low_problem_index = i
+                low_index_within_problem = low - previous_index_sum
+                low_found = True
+            if paragraph_pair_index >= high:
+                high_problem_index = i
+                high_index_within_problem = min(low_index_within_problem + self.batch_size, self.__len__())
+                high_found = True
+                break
 
-    def __getitem__(self, idx, force_compute=False):
-        idx_path = self.file_path / f"{idx}.npz"
-        # print(f"Pan21PyDataset {idx_path=}")
-        if force_compute or not idx_path.exists():
-            # print(f"{idx=}")
-            # Return x, y for batch idx.
-            low = idx * self.batch_size
-            # Cap upper bound at array length; the last batch may be smaller
-            # if the total number of items is not a multiple of batch size.
-            high = min(low + self.batch_size, len(self.y))
-            # print(f"{low=} {high=}")
-            batch_x, batch_y = Pan21PyDataset.get_data(self.x_path, self.y, low, high, self.max_input_length)
+            previous_index_sum = paragraph_pair_index
 
-            # print(f"{batch_x[0].shape=}")
-            # print(f"{len(batch_x)=}")
-            # print(f"{len(batch_y)=}")
-        else:
-            # print(f"file exists")
-            npzfile = np.load(idx_path)
-            batch_x = npzfile['batch_x']
-            batch_y = npzfile['batch_y']
+        batch_x, batch_y = self.get_data(low_problem_index, high_problem_index, low_index_within_problem, high_index_within_problem)
 
         assert batch_x.shape[0] != 0, f"{idx=}: Dimension is 0 {batch_x.shape=} {batch_y.shape=}"
         assert batch_x.shape[0] == batch_y.shape[0], f"{idx=}: Dimension mismatch {batch_x.shape=} {batch_y.shape=}"
@@ -323,25 +315,21 @@ def free_memory():
 from scipy.signal import butter, lfilter
 # from cupyx.scipy.signal import lfilter
 
-class Pan21FourierFilterDataset(Pan21FourierDataset):
-    def __init__(self, x_set, y_set, file_path,  cutoff_frequencies, **kwargs):
-        super().__init__(x_set, y_set, file_path, **kwargs)
+class Pan21FourierFilterDataset(Pan21PyDataset):
+    def __init__(self, x_set, y_set,  cutoff_frequencies, **kwargs):
+        super().__init__(x_set, y_set, **kwargs)
         
-        lowcut, highcut = cutoff_frequencies
         order = 5
-        nyq = 512 / 2
-        low = lowcut / nyq
-        high = highcut / nyq
+        # clamp freqencies
+        cutoff_frequencies = [max(cutoff_frequencies[0], .001), min(cutoff_frequencies[1], .999)]
         
-        self.i, self.u = butter(order, [low, high], btype='bandstop')
+        self.i, self.u = butter(order, cutoff_frequencies, btype='bandstop')
     
     def __getitem__(self, idx, force_compute=False):
-        idx_path = self.file_path / "fourier" / f"{idx}.npz"
+        batch_x, batch_y = super().__getitem__(idx)
 
-        npzfile = np.load(idx_path)
-        fourier_batch_x = npzfile['fourier_batch_x']
+        _, j, _ = batch_x.shape
+        batch_x[:, :j//2, :] = lfilter(self.i, self.u, batch_x[:, :j//2, :])
+        batch_x[:, j//2:, :] = lfilter(self.i, self.u, batch_x[:, j//2:, :])
 
-        # y = np.zeros(fourier_batch_x.shape)
-        # for i in range(fourier_batch_x.shape[0]):
-        #     y[i] = lfilter(self.i, self.u, fourier_batch_x[i])
-        return lfilter(self.i, self.u, fourier_batch_x)
+        return batch_x, batch_y
