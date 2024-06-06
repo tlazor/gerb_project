5105dab821af1475699608611226e040de529f3d
Maksym Kravchuk <max.kravchuk@approvedadmissions.com>
2024-06-06 23:26:07 +0200

    WE are close. Bye!

--- run_evaluation.py
+++ run_evaluation.py
@@ -1,68 +1,31 @@
-from pan21_functions import *
 import json
 import math
-import tensorflow as tf
-import evaluator
-import pandas as pd
-import matplotlib.pyplot as plt
+import evaluation.evaluator
 from pathlib import Path
 import pandas as pd
-from pathlib import Path
 import matplotlib.pyplot as plt
 import re
-from natsort import natsorted 
+from natsort import natsorted
+import argparse
+import os
 
-def load_results():
-    with open('model_results.json', 'r') as json_file:
-        data = json.load(json_file)
-    return data
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process a json results directory.')
-    parser.add_argument('--jsons', type=str, default='/default/path', help='The path to the models folder')
-    args = parser.parse_args()
-
-    if not os.path.isdir(args.jsons):
-        print(f"The models folder {args.jsons} does not exist.")
-    else:
-        print(f"Processing models in folder: {args.jsons}")
-
-    model_dir = Path(args.jsons)
-
-    for model_path in model_dir.glob("*.json"):
-        task1_result = evaluator.compute_score_single_predictions(truth, dict_of_jsons_result, 'multi-author')
-        task2_result = evaluator.compute_score_multiple_predictions(truth, dict_of_jsons_result, 'changes', labels=[0, 1])
-        task3_result = evaluator.compute_score_multiple_predictions(truth, dict_of_jsons_result, 'paragraph-authors', labels=[1, 2, 3, 4])
-
-        # Append scores to the lists
-        task1_scores.append(task1_result)
-        task2_scores.append(task2_result)
-        task3_scores.append(task3_result)
-
-        # Create a DataFrame to store the evaluation results
-        results_df = pd.DataFrame({
-            'Model': model_path.stem,
-            'Task 1 Score': task1_scores,
-            'Task 2 Score': task2_scores,
-            'Task 3 Score': task3_scores
-        })
-
-        # Display the results DataFrame
-        print(results_df)
-
+def print_logs(model_dir, isFourier = True):
     log_dfs = []
 
     # Loop through sorted log files
     for log_path in natsorted(model_dir.glob('*.log')):
-        num_regex = "\d*(?=_)"
+        if isFourier:
+            num_regex = "ff_\d*(?=_)"
+        else:
+            num_regex = ".*_.*(?=_)"
 
-        # Extracting the number of Fourier features from the filename
-        num_fourier_features = re.search(num_regex, log_path.stem)[0]
+        # Extracting the number of features from the filename
+        features = re.search(num_regex, log_path.stem)[0]
 
         # Reading the log file
         log_df = pd.read_csv(log_path, sep=',', engine='python')
         log_df.columns = ["epoch", "auc", "binary_accuracy", "loss", "val_auc", "val_binary_accuracy", "val_loss"]
-        log_df["num_fourier"] = [num_fourier_features] * len(log_df["epoch"])
+        log_df["num_features"] = [features] * len(log_df["epoch"])
         log_dfs.append(log_df)
 
     # Concatenate all DataFrame into one
@@ -79,8 +42,8 @@ if __name__ == "__main__":
         plt.figure()
         
         # Group by number of Fourier features and plot each group with different line styles
-        for (num_fourier, group_df), line_style in zip(df.groupby("num_fourier"), line_styles):
-            group_df.plot(x="epoch", y=metric, label=f'{num_fourier}', ax=plt.gca(), linestyle=line_style)
+        for (num_features, group_df), line_style in zip(df.groupby("num_features"), line_styles):
+            group_df.plot(x="epoch", y=metric, label=f'{num_features}', ax=plt.gca(), linestyle=line_style)
 
         # Adding labels and title
         plt.xlabel("Epoch")
@@ -91,3 +54,44 @@ if __name__ == "__main__":
         
         # Display the plot
         plt.show()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process a json results directory.')
+    parser.add_argument('--model', type=str, default='/default/path', help='The path to the models folder')
+    args = parser.parse_args()
+
+    if not os.path.isdir(args.model):
+        print(f"The models folder {args.model} does not exist.")
+    else:
+        print(f"Processing models in folder: {args.model}")
+
+    # for model_path in Path(args.model).glob("*.json"):
+
+    #     with open(model_path, 'r') as json_file:
+    #         dict_of_jsons_result = json.load(json_file)
+
+    #     task1_result = evaluator.compute_score_single_predictions(truth, dict_of_jsons_result, 'multi-author')
+    #     task2_result = evaluator.compute_score_multiple_predictions(truth, dict_of_jsons_result, 'changes', labels=[0, 1])
+    #     task3_result = evaluator.compute_score_multiple_predictions(truth, dict_of_jsons_result, 'paragraph-authors', labels=[1, 2, 3, 4])
+
+    #     # Append scores to the lists
+    #     task1_scores.append(task1_result)
+    #     task2_scores.append(task2_result)
+    #     task3_scores.append(task3_result)
+
+    #     # Create a DataFrame to store the evaluation results
+    #     results_df = pd.DataFrame({
+    #         'Model': model_path.stem,
+    #         'Task 1 Score': task1_scores,
+    #         'Task 2 Score': task2_scores,
+    #         'Task 3 Score': task3_scores
+    #     })
+
+    #     # Display the results DataFrame
+    #     print(results_df)
+
+    # Fourier
+    print_logs(Path(args.model))
+
+    # Filter
+    print_logs(Path(args.model), False)
